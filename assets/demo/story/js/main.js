f7c39eb4c7828080b7dcc7f6b486047c18817771
eugene-it <eugeneit@mail.ru>
2014-08-14 14:56:13 +0300

    memory

---
+++ assets/demo/story/js/main.js
@@ -0,0 +1,34 @@
+/* SCROLL */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });                   
+});
+
+  /* LOADER */
+  $(window).on('load', function () {
+    var $preloader = $('#page'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(800).fadeOut('slow');
+ });
+/*parallax*/
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+ 
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('.background_hero').css('top',(0-(scrolled*.5))+'px');
+}
+
+
